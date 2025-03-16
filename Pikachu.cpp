fcc63e1218b0afe3d0a18dca5b594ca5b8c42fbe
Linh18082006 <nguyenquanglinh18082006@gmail.com>
2025-03-17 02:06:01 +0700

    Initial commit.

--- Pikachu.cpp
+++ Pikachu.cpp
@@ -583,7 +583,6 @@ bool isRunning = true, isRun = 0;
 
 bool isLeaderboardOpen = false;
 bool isTutorial = false;
-bool click_tutorial = 0;
 
 bool next = 0;
 
@@ -880,7 +879,7 @@ void HandleEvents(SDL_Event &event) {
             int x, y;
             SDL_GetMouseState(&x, &y);
             if (x >= buttonPlay.x && x <= buttonPlay.x + buttonPlay.w && y >= buttonPlay.y && y <= buttonPlay.y + buttonPlay.h) {
-                if(playerName.size() != 0 && enterPressed == 1 && click_tutorial == 1)
+                if(playerName.size() != 0 && enterPressed == 1)
                 {
                     Mix_PlayChannel(-1, sound_click, 0);
                     Mix_VolumeChunk(sound_click, 128);
